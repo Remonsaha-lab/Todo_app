1a26a083a2efe3e7408d027fc0f28307573c5a88
Remon s28 <remonsaha95@gmail.com>
2025-12-31 14:59:18 +0530

    Fix broken login redirect in todo.js

--- todo.js
+++ todo.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const token = localStorage.getItem("token");
     if (!token) {
         alert("Please login first");
-        window.location.href = "../login_page/login.html";
+        window.location.href = "/login";
         return;
     }
 
